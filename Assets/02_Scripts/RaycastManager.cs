f3ebeac61bb7a92a651755987a7b0799a34ff923
Lee JaeHyun <myleje@gmail.com>
2026-07-07 12:36:42 +0900

    fix: change CreateAnchor method to synchronous

--- Assets/02_Scripts/RaycastManager.cs
+++ Assets/02_Scripts/RaycastManager.cs
@@ -43,7 +43,7 @@ public class RaycastManager : MonoBehaviour
         }
     }
 
-    async void CreateAnchor(ARRaycastHit hit)
+    void CreateAnchor(ARRaycastHit hit)
     {
         ARAnchor anchor = null;
         
